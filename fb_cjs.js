940f4f6a07483b89fc01756c1cf871fd2adecb1a
Andrew Chen <achen88@mit.edu>
2020-01-10 21:04:51 +0900

    initial presets

---
+++ fb_cjs.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(".fbChatSidebarBody").css("height", "100%");
+  $(".fbChatTypeahead").css("bottom", "-1px");
+  $(".noGrip").css("padding", "0 0 67px");
+  $("._64b").css("padding", "0");
+});
